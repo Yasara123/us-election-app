b7595caaafd0cd65bb6e12e1069a1815c33c5906
Yasara <yasara@wso2.com>
2016-04-15 02:22:55 +0530

    app v1

---
+++ js/main.js
@@ -0,0 +1,21 @@
+    // Document on load.
+    $(function() {
+        var newColour = ["#fa574b", "#3ec2ee", "#e51000", "#1c40fb"];
+        ajaxCountD();
+        ajaxCountR();
+        setInterval(ajaxCountD, 1000);
+        setInterval(ajaxCountR, 1000);
+        ajaxPopular("Trump");
+        PopularElection("Trump");
+        ajaxPopularLink();
+        ajaxNews();
+        ajaxLatest();
+        getPersonDataCloud("WCR1", "TRUMP", newColour[0]);
+        getPersonDataCloud("WCD1", "CLINTON", newColour[1]);
+        getPersonDataCloud("WCD2", "BERNIE", newColour[3]);
+        getPersonDataCloud("WCR2", "CRUZ", newColour[2]);
+        setInterval(ajaxLatest, 5000);
+        ajaxGarphSentiment("js/jaggery-scripts/SentimetGraphServer.jag", "11", "22", "33");
+        drawCommunityGraph("communityGraph");
+
+    });
